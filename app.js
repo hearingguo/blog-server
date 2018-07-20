3b6c8a333a2793cf80362611d4c3ef13751b864c
hearingguo <hearingguo@gmail.com>
2018-07-20 08:47:52 +0800

    测试玩的很是high

--- app.js
+++ app.js
@@ -15,11 +15,28 @@ mongoosePaginate.paginate.options = {
 	limit: config.APP.LIMIT
 }
 
+const handler = async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+    ctx.response.status = err.statusCode || err.status || 500;
+    ctx.response.type = 'html';
+    ctx.response.body = '<p>Something wrong, please contact administrator.</p>';
+    ctx.app.emit('error', err, ctx);
+  }
+};
+
 const main = ctx => {
-  console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`);
-  ctx.response.body = 'Hello World1111111';
+  ctx.throw(500)
 }
 
+app.on('error', function(err) {
+  console.log('logging error ', err.message);
+  console.log(err);
+});
+
+app.use(handler)
 app.use(main)
+
 
 app.listen(3000)
